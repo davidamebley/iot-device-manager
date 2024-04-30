0fe3e2d5a237b29466ca9441c0f68a6c01b383bd
David Amebley <davameb@gmail.com>
2024-04-30 05:32:19 +0300

    Initial project setup with Spring Boot

---
+++ src/main/java/com/iotserver/iotdevicemanager/IoTDeviceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.iotserver.iotdevicemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTDeviceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoTDeviceManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iotserver/iotdevicemanager/IoTDeviceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iotserver.iotdevicemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTDeviceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
