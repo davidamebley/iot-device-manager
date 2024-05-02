2e6e8b4998b22fb7084a1847b9894624e6d076cd
David Amebley <davameb@gmail.com>
2024-05-02 15:25:23 +0300

    Create an initial DeviceService class

---
+++ src/main/java/com/iotserver/iotdevicemanager/service/DeviceService.java
@@ -0,0 +1,13 @@
+package com.iotserver.iotdevicemanager.service;
+
+import com.iotserver.iotdevicemanager.repository.DeviceRepository;
+
+public class DeviceService {
+    private final DeviceRepository deviceRepository;
+
+    public DeviceService(DeviceRepository deviceRepository) {
+        this.deviceRepository = deviceRepository;
+    }
+
+//    TODO: Service methods for device management
+}
