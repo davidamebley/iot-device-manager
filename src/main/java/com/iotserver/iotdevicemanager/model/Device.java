59c10739bc3c678a5730d48ba3a26c2122ce9c8a
David Amebley <davameb@gmail.com>
2024-05-02 15:38:45 +0300

    Create getters and setters for Device model fields

--- src/main/java/com/iotserver/iotdevicemanager/model/Device.java
+++ src/main/java/com/iotserver/iotdevicemanager/model/Device.java
@@ -13,4 +13,70 @@ public class Device {
     private String type;
     private String location;
     private String status;
+
+    public Device(Long id, String deviceId, String name, String type, String location, String status) {
+        this.id = id;
+        this.deviceId = deviceId;
+        this.name = name;
+        this.type = type;
+        this.location = location;
+        this.status = status;
+    }
+
+    public Device() {}
+
+    public Device(String name, String type, String location, String status) {
+        this.name = name;
+        this.type = type;
+        this.location = location;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
