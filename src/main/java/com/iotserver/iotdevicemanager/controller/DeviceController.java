de0498449b1f8dbcbd1322b83a5c3d2b7ce6c90d
David Amebley <davameb@gmail.com>
2024-05-17 17:58:33 +0300

    Create Device Registration Endpoint in DeviceController.java

--- src/main/java/com/iotserver/iotdevicemanager/controller/DeviceController.java
+++ src/main/java/com/iotserver/iotdevicemanager/controller/DeviceController.java
@@ -1,7 +1,11 @@
 package com.iotserver.iotdevicemanager.controller;
 
+import com.iotserver.iotdevicemanager.model.Device;
 import com.iotserver.iotdevicemanager.service.DeviceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +17,11 @@ public class DeviceController {
     @Autowired
     public DeviceController(DeviceService deviceService) {
         this.deviceService = deviceService;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Device> registerDevice(@RequestBody Device device){
+        Device savedDevice = deviceService.registerDevice(device);
+        return ResponseEntity.ok(savedDevice);
     }
 }
